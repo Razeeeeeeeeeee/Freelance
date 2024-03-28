c8bdeba8bfba1e354ee4f77834bb90ea495c5c8c
Razeen <razeen.pers@gmail.com>
2024-03-29 03:32:09 +0530

    Added Navbar support

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import  Candidate  from './pages/candidate'
 import Home from './pages/home'
+import NavBar from './components/navbar'
 
 
 function App() {
@@ -9,11 +10,13 @@ function App() {
   return (
     <>
      <BrowserRouter>
+    <div className="relative"><NavBar /></div>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/candidate' element={<Candidate />} />
         </Routes>
      </BrowserRouter>
+
     </>
   )
 }
