7c165cfeb6c442fa62a87e1001bd5bbff213fb5d
Razeen <razeen.pers@gmail.com>
2024-04-05 23:37:43 +0530

    Added support for navigation

--- frontend/src/pages/candidate.jsx
+++ frontend/src/pages/candidate.jsx
@@ -14,13 +14,15 @@ export default function(){
             <div className="font-bold text-3xl">
                 Candidate Description
             </div>
-            <div className="flex justify-center items-baseline pt-20 pr-10 pl-10 align-baseline px-20">
+            <div className="flex justify-center items-baseline pt-16 pr-10 pl-10 align-baseline px-20">
                 <div className="p-0 m-0 pr-10 "> Upload an excel file containing the job details</div>
-                <form>
-                <input className="file-input w-full max-w-xs rounded-lg text-xs" id="f" type="file"></input>
-                </form>
+                <button className="btn btn-ghost m-5 px-3 py-2 rounded-lg" form="file_upload">Submit</button> 
             </div>
+            <form id="file_upload">
+                <div className="flex justify-center mt-0 p-0"><input className="flex file-input w-full max-w-xs rounded-lg text-xs " id="f" type="file"></input></div>
+            </form>
             <div className="pt-10 font-light text-gray-400 text-sm">If you want to enter the details manually, <Link className="underline text-zinc-900 " to="/candidate_manual">Click here!</Link> </div>
+            
       </Card>
         </>
     )
