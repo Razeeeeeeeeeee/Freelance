20229948b041cc6f2a102428816779a422e00a36
Razeen <razeen.pers@gmail.com>
2024-04-12 11:29:08 +0530

    Fixed django admin page

--- backend/backend/User/admin.py
+++ backend/backend/User/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Employer
+from .models import Employer,Candidate
 # Register your models here.
 admin.site.register(Employer)
+admin.site.register(Candidate)
