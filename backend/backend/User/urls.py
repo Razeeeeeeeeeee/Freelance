11111501369f9b0fb599aea552b9f6caddd67b51
ArkaRutvik <96034045+demigod-22@users.noreply.github.com>
2024-07-30 15:15:02 +0530

    added adaptive algo

--- backend/backend/User/urls.py
+++ backend/backend/User/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Employer_view,Candidate_view,Run_Simulation_ms_sc_gdc,Run_simulation_ms_sc_greedy
+from .views import Employer_view,Candidate_view,Run_Simulation_ms_sc_gdc,Run_simulation_ms_sc_greedy,Run_simulation_ms_sc_adaptive
 
 urlpatterns = [
     path('employer/', Employer_view.as_view()),
     path('candidate/', Candidate_view.as_view()),
     path('get_ms_sc_gdc/',Run_Simulation_ms_sc_gdc.as_view()),
     path('get_ms_sc_greedy/',Run_simulation_ms_sc_greedy.as_view()),
+    path('get_ms_sc_adaptive/',Run_simulation_ms_sc_adaptive.as_view()),
 ]
