8bfabc80a40774e914e54b27491896f0119e00b2
ArkaRutvik <96034045+demigod-22@users.noreply.github.com>
2024-07-30 15:36:29 +0530

    added algos

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from django.core.files import File
 from django.http import HttpResponse
 
+import math
+import numpy as np
+from ..User.models import Employer,Candidate
+
 # Create your views here.
 from rest_framework import status
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
@@ -138,6 +142,219 @@ class RunSimulation(APIView):
             os.path.join(self.candidate_dir, self.getfiles(self.candidate_dir))
         )
 
+    def distance(self,loc1,loc2):
+        loc1 = [int(a) for a in loc1.split(',')]
+        loc2 = [int(a) for a in loc2.split(',')]
+        return math.dist(loc1,loc2)
+
+    def check_skillmatch(self,worker,task):
+        k = set(worker.skills)&set(task.skills)
+        if(k == None):
+            return False
+        else:
+            return True
+
+    def check_valid(self,worker,task):
+        if((self.check_skillmatch(worker,task)) & (self.distance(worker.location,task.location)*(worker.velocity) <= (task.arrival_deadline)) & (self.distance(worker.location,task.location)*(worker.velocity)<=(task.arrival_deadline))):
+            return True
+        return False
+
+    def generate_graph(self,list_of_tasks,list_of_workers):
+        k = {}
+        worker_no=-1
+        for i in list_of_workers:
+            worker_no = worker_no + 1
+            for j in range(len(list_of_tasks)):
+                if(self.check_valid(i,list_of_tasks[j])):
+                    if i in k.keys():
+                        k[i].append(list_of_tasks[j])
+                    else:
+                        k[i] = [list_of_tasks[j]]
+        return k
+
+    def calculate_score(self,task,worker):
+        s = (task.budget - worker.cost)*len(list(set(worker.skills).intersection(set(task.skills))))
+        s = s/len(task.skills)
+        return s
+
+    def ms_sc_greedy(self,list_of_workers,list_of_tasks):
+        Ip = []
+        k = self.generate_graph(list_of_workers,list_of_tasks)
+        print(f"graph : {k}")
+        cnt = 0
+        while((len(list_of_tasks) !=0) & (len(list_of_workers)!=0)):
+            S_cand = []
+            assigned_w = 0
+            for task in list_of_tasks:
+                print(task)
+                for worker in k[task]:
+                    S_cand.append([worker,task])
+                score = 0
+                for g in S_cand:
+                    cur_score = self.calculate_score(g[0],g[1])
+                    if cur_score>score:
+                        score = cur_score
+                        assigned_w = g[0]
+                        assigned_t = g[1]
+            if(assigned_w != 0):
+                Ip.append([assigned_w.name,assigned_t.name,cur_score])
+                if assigned_w in list_of_workers:
+                    list_of_workers.remove(assigned_w) 
+                skills_covered = list(set(assigned_w.skills).intersection(set(assigned_t.skills)))
+                assigned_t.skills = [ele for ele in assigned_t.skills if ele not in skills_covered]
+            if cnt == 8:
+                break
+            cnt += 1
+        return Ip
+    
+
+    def generate_graph1(self,list_of_workers,list_of_tasks):
+        k = {}
+        worker_no=-1
+        for i in list_of_workers:
+            worker_no = worker_no + 1
+            for j in range(len(list_of_tasks)):
+                if(self.check_valid(i,list_of_tasks[j])):
+                    if list_of_tasks[j] in k.keys():
+                        k[list_of_tasks[j]].append(i)
+                    else:
+                        k[list_of_tasks[j]] = [i]
+        return k
+
+    def calculate_degree(graph):
+        count1 = 0
+        count2 = 0
+        for i in graph.keys():
+            count1 = count1 + 1
+            count2 = count2 + len(graph[i])
+        degree = count2/count1
+        return degree
+
+    def calculate_g(m,degree):
+        g=2
+        l = ((m*(math.log10(m))*(g*math.log10(g) - g - 1))/(g*math.log10(2*g))) + ((1-m)/((1-g)**2))*(degree**2)
+        while(l<0):
+            g = g+1
+            if(g==m):
+                break
+            l =  l = ((m*(math.log(m))*(g*math.log(g) - g - 1))/(g*math.log(2*g))) + ((1-m)/((1-g)**2))*(degree**2)
+        return g
+
+    def find_nearest_tasks(self,task,list_of_tasks,g):
+        nearest_tasks = []
+        distances = {}
+        l = math.ceil(len(list_of_tasks)/g) 
+        l = min(l,len(list_of_tasks))
+        for i in list_of_tasks:
+            loc = self.distance(task.location,i.location)
+            distances[i] = loc
+        keys = list(distances.keys())
+        values = list(distances.values())
+        sorted_value_index = np.argsort(values)
+        sorted_tasks = [keys[i] for i in sorted_value_index]
+        nearest_tasks = sorted_tasks[:l]
+        return nearest_tasks
+
+
+    def ms_sc_decomp(self,list_of_workers,list_of_tasks,g):
+        P = []
+        for i in range(g):
+            P.append([])
+        G = self.generate_graph1(list_of_tasks,list_of_workers)
+        print("Graph : ",G)
+        for s in range(g):
+            small = float('inf')
+            max_index = 0
+            cur_index = 0
+            print("list of tasks : ",list_of_tasks)
+            for y in list_of_tasks:
+                if y.location[1] < small:
+                    small = y.location[1]
+                    max_index = cur_index
+                cur_index = cur_index+ 1
+            anchor = list_of_tasks[max_index]
+            nearest_tasks = self.find_nearest_tasks(anchor,list_of_tasks,g)
+            print("anchor : ",anchor)
+            print("nearest tasks : ",nearest_tasks)
+            sub_prob = {}
+            for t in nearest_tasks:
+                sub_prob[t] = G[t]
+            P[s] = sub_prob
+            list_of_tasks = [i for i in list_of_tasks if i not in nearest_tasks]
+        return P
+
+    def ms_sc_conflict_reconcile(Ip,Ips):
+        worker_Ip = [i[0] for i in Ip]
+        worker_Ips = [i[0] for i in Ips]
+        conflicting_workers = list(set(worker_Ips).intersection(set(worker_Ip)))
+        while(len(conflicting_workers)!=0):
+            k=0
+            max_index = 0
+            max = 0
+            for i in conflicting_workers:
+                if i.cost>max:
+                    max = i.cost
+                    max_index = k
+                k = k+1
+            highest_costworker = conflicting_workers[max_index]
+            conflicting_workers.remove(highest_costworker)
+        return Ip
+
+    def ms_sc_gdc(self,list_of_tasks,list_of_workers):
+        Ip = []
+        graph = self.generate_graph1(list_of_tasks,list_of_workers)
+        degree = self.calculate_degree(graph)
+        g = self.calculate_g(len(list_of_tasks),degree)
+        sub_problems = self.ms_sc_decomp(list_of_workers,list_of_tasks,g)
+        for i in sub_problems:
+            if len(graph[i]) != 1:
+                Ip[i] = self.ms_sc_gdc(i,graph[i])
+            else:
+                Ip[i] = self.ms_sc_greedy(i,graph[i])
+        Sol = []
+        for i in range(1,g+1):
+            Sol = self.ms_sc_conflict_reconcile(Sol,Ip[i])
+        return Sol
+            
+
+    def calc_costgreedy(self,list_of_tasks,list_of_workers):
+        G = self.generate_graph(list_of_tasks,list_of_workers)
+        G1 = self.generate_graph1(list_of_tasks,list_of_workers)
+        degw =self.calculate_degree(G)
+        degt = self.calculate_degree(G1)
+        m = len(list_of_tasks)
+        n = len(list_of_workers)
+        costg = m*n + n*degt*(3*m + degw) + m*degt**2
+        return costg
+
+    def calc_costgdc(n,degree,m,g,list_of_tasks,list_of_workers):
+        m = len(list_of_tasks)
+        n = len(list_of_workers)
+        return (m*g+n)(degree-1)
+
+
+    def ms_sc_adaptive(self,list_of_workers,list_of_tasks):
+        Ip = []
+        cost_greedy = self.calc_costgreedy(list_of_tasks,list_of_workers)
+        cost_gdc = self.calc_costgdc()
+        if cost_gdc < cost_greedy:
+            Ip = self.ms_sc_greedy(list_of_workers,list_of_tasks)
+        else:
+            graph = self.generate_graph1(list_of_tasks,list_of_workers)
+            degree = self.calculate_degree(graph)
+            g = self.calculate_g(len(list_of_tasks),degree)
+            sub_problems = self.ms_sc_decomp(list_of_workers,list_of_tasks,g)
+            Ips = []
+            for i in range(g):
+                Ips.append([])
+            for i in sub_problems : 
+                Ips[i] = self.ms_sc_adaptive(i[0],i[1])
+            for i in range(g):
+                Ip = self.ms_sc_conflict_reconcile(Ip,Ips[i])
+        return Ip
+
+    
+
     def getfiles(self, dirpath):
         a = [s for s in os.listdir(dirpath) if os.path.isfile(os.path.join(dirpath, s))]
         a.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)))
@@ -230,6 +447,49 @@ class RunSimulation(APIView):
             # candidate_file = candidate_file.sample(frac=1)
             # employee_file = employee_file.sample(frac=1)
             # min_size = 10
+
+
+        if request.data["method"] == "greedy":
+            list_of_tasks = [tasks for tasks in Employer.objects.all()]
+            list_of_workers = [workers for workers in Candidate.objects.all()]
+            output  =  self.ms_sc_greedy(list_of_tasks,list_of_workers)
+            #output = [['rut','frontend',100]]
+            results = [[i[0],i[1]] for i in output]
+            scores = [i[2] for i in output]
+            response = {
+                    "results": results,
+                    "happiness": scores,
+                }
+            return Response(json.dumps(response), status=status.HTTP_200_OK)
+
+        if request.data["method"] == "gdc":
+            list_of_tasks = [tasks for tasks in Candidate.objects.all()]
+            list_of_workers = [workers for workers in Employer.objects.all()]
+            output  =  self.ms_sc_gdc(list_of_tasks,list_of_workers)
+            #output = [['rut','frontend',100]]
+            results = [[i[0],i[1]] for i in output]
+            scores = [i[2] for i in output]
+            response = {
+                    "results": results,
+                    "happiness": scores,
+                }
+            return Response(json.dumps(response), status=status.HTTP_200_OK)
+
+        
+        if request.data["method"] == "adaptive":
+            list_of_tasks = [tasks for tasks in Employer.objects.all()]
+            list_of_workers = [workers for workers in Candidate.objects.all()]
+            output  =  self.ms_sc_greedy(list_of_tasks,list_of_workers)
+            #output = [['rut','frontend',100]]
+            results = [[i[0],i[1]] for i in output]
+            scores = [i[2] for i in output]
+            response = {
+                    "results": results,
+                    "happiness": scores,
+                }
+            return Response(json.dumps(response), status=status.HTTP_200_OK)
+
+
 
         # candidate_file = candidate_file.iloc[:min_size]
         # candidate_file.reset_index(drop=True,inplace = True)
